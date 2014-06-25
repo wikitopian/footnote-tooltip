590daf259848c0f49f691a96444637d7d8b0775d
wikitopian <matt@swarmstrategies.com>
2014-06-25 17:26:46 +0000

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+Footnote Tooltip
+================
+
+Add a tooltip to WordPress MarkDown footnotes
+
+Installation
+------------
+
+    The usual.
---
+++ footnote-tooltip.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Plugin Name: Footnote Tooltip
+ * Plugin URI: http://www.github.com/wikitopian/footnote-tooltip
+ * Description: Add a tooltip to WordPress MarkDown footnotes
+ * Version: 0.1
+ * Author: @wikitopian
+ * Author URI: http://www.github.com/wikitopian
+ * License: GPLv3
+ * */
+
+class Footnote_Tooltip {
+
+	public function __construct() {
+
+	}
+}
+$footnote_tooltip = new Footnote_Tooltip();
